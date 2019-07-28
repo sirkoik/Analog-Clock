e589f121020bec72a250fbfb0d414cc5eb1dfbd3
Alexander Koik-Cestone <akoikcestone@gmail.com>
2019-07-28 17:54:18 -0400

    0.0.2
    Changed clock face to randomly colorize on load / tap

--- scripts/analog-clock.js
+++ scripts/analog-clock.js
@@ -1,10 +1,12 @@
-const VERSION = '0.0.1';
+const VERSION = '0.0.2';
 
 var running = true;
 var roundedMinSec = false;
 
 //var tickTime = 200;
 //window.onload = () => setInterval(() => tick(), tickTime);
+
+window.onload = () => colorize();
 
 function tick() {
     if (!running) return false;
@@ -27,13 +29,15 @@ function tick() {
     document.querySelector('.hand-minute').style.transform = 'rotate('+minRotation+'deg)';
     document.querySelector('.hand-second').style.transform = 'rotate('+secRotation+'deg)';
     
-    
+    // text on hands.
     document.querySelector('.hand-hour').textContent = sig3(Math.round(hr * 1000) / 1000);
     document.querySelector('.hand-minute').textContent = sig3(Math.round(min * 1000) / 1000);
     document.querySelector('.hand-second').textContent = sig3(sec);
     window.requestAnimationFrame(tick);
 }
 
+// sig3: keep the number of digits constant so that the numbers don't jump around
+// addLeadingZero: add a zero to all single-digit leading numbers
 var addLeadingZero = false;
 function sig3(num) {
     num = num + '';
@@ -59,14 +63,41 @@ window.requestAnimationFrame(tick);
 
 document.onclick = () => {
     //roundedMinSec = !roundedMinSec;
-    toggleStyle();
+    //toggleStyle();
+    colorize();
 }
 
 document.title += ' v' + VERSION;
 
+
+// toggleStyle: toggle between black/white style (disabled since 0.0.2)
 var ssHrefs = ['bg-white.css', 'bg-black.css'];
 var styleIndex = 0;
 function toggleStyle() {
     styleIndex = styleIndex >= 1? 0 : 1
     document.getElementById('colors').href = 'styles/' + ssHrefs[styleIndex];
+}
+
+// colorize: Give the watch face, hands and random colors that aren't too close to one another.
+function colorize() {
+    var foreColor = 0;
+    var backColor = 0;
+
+    while(Math.abs(foreColor - backColor) < 10) {
+        foreColor = Math.random() * 255;
+        backColor = Math.random() * 255;
+    }
+
+    var squares = document.querySelectorAll('.square');
+    for (square of squares) {
+        square.style.backgroundColor = 'hsl('+foreColor+',50%,50%)';
+    }
+
+    var hands = document.querySelectorAll('.hand');
+    for (hand of hands) {
+        hand.style.backgroundColor = 'hsl('+foreColor+',20%,50%)';
+    }
+
+    backColor = Math.floor(backColor);
+    document.body.style.backgroundImage = 'linear-gradient(to bottom right, hsl('+backColor+',50%,80%), hsl('+backColor+',50%,50%))';
 }
