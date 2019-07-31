89df59ab816e4375eb3b4ab39a1718ef059aa0b5
Alexander Koik-Cestone <akoikcestone@gmail.com>
2019-07-31 14:34:19 -0400

    0.0.4
    Major restructuring of code into classes.
    
    State is now saved in URL.

---
+++ scripts/AnalogClock.js
@@ -0,0 +1,137 @@
+// AnalogClock class
+class AnalogClock {
+    running = false;
+    directDrive = false; // second and minute hands move in intervals instead of smoothly.
+
+    numberType = -1;
+    clockMath;
+
+    constructor(args) {
+        if (args) {
+            if (args.directDrive) this.directDrive = args.directDrive;
+        }
+
+        this.clockMath = new ClockMath(args);
+
+        // get parameters from query string.
+        const params = new URLSearchParams(location.search);
+        var type = parseInt(params.get('mode'));
+        var foreColor = params.get('fg');
+        var backColor = params.get('bg');
+        
+        if (this.isNumeric(type)) {
+            if (type < 0) type = 0;
+            this.numberType = type;
+            this.clockMath.numberType = type;
+            this.setNumbers(type);
+        } else {
+            this.setNumbersRand();
+        }
+
+        if (this.isNumeric(backColor) && this.isNumeric(foreColor)) {
+            this.setColors(foreColor, backColor);
+        } else {
+            this.setColorsRand();
+        }
+        
+        document.onclick = () => {
+            this.setNumbersRand();
+            this.setColorsRand();
+        }
+    }
+
+    // runClock: get the clock started.
+    runClock() {
+        this.running = true;
+        this.setTime();
+        window.requestAnimationFrame(() => this.setTime());
+    }
+
+    // setTime: The animation function.
+    // note on es6 classes and requestAnimationFrame https://stackoverflow.com/questions/49197700/es6-class-this-in-callback-of-requestanimationframe
+    setTime() {
+        if (!this.running) return false;
+
+        var t = this.clockMath.getHMS();
+        
+        // position hands.
+        document.querySelector('.hand-hour').style.transform = 'rotate('+t.hrRotation+'deg)';
+        document.querySelector('.hand-minute').style.transform = 'rotate('+t.minRotation+'deg)';
+        document.querySelector('.hand-second').style.transform = 'rotate('+t.secRotation+'deg)';
+        
+        // add text to hands.
+        var hNum = this.clockMath.numOnHands(t.hr, t.min, t.sec);
+
+        document.querySelector('.hand-hour').textContent = hNum[0];
+        document.querySelector('.hand-minute').textContent = hNum[1];
+        document.querySelector('.hand-second').textContent = hNum[2];
+
+        window.requestAnimationFrame(() => this.setTime());        
+    }
+
+    fillNumbers(numbers) {
+        var squares = document.querySelectorAll('.square');
+        // squares.length - 1 avoids overwriting the 'hub' square with its link
+        for (var x = 0; x < squares.length - 1; x++) {
+            squares[x].textContent = numbers[x];
+        }        
+    }
+
+    setNumbers(number) {
+        const numbers = this.clockMath.getFaceNumbers(number);
+        this.numberType = this.clockMath.numberType;
+        this.fillNumbers(numbers);
+    }
+
+    setNumbersRand() {
+        const numbers = this.clockMath.getFaceNumbersRand();
+        this.numberType = this.clockMath.numberType;
+        this.fillNumbers(numbers);
+    }
+
+    // setColors: Color watch face and hands.
+    setColors(foreColor, backColor) {
+        var squares = document.querySelectorAll('.square');
+        for (var square of squares) {
+            square.style.backgroundColor = 'hsl('+foreColor+',50%,50%)';
+        }
+
+        var hands = document.querySelectorAll('.hand');
+        for (var hand of hands) {
+            hand.style.backgroundColor = 'hsl('+foreColor+',20%,50%)';
+        }
+
+        var backColorFlr = Math.floor(backColor);
+        document.body.style.backgroundImage = 'linear-gradient(to bottom right, hsl('+backColorFlr+',50%,80%), hsl('+backColorFlr+',50%,50%))';
+    
+        this.replaceHistState(foreColor, backColor, this.numberType);
+    }
+
+    // setColorsRand: Color watch face and hands with random colors
+    // that are reasonably complimentary.
+    setColorsRand() {
+        var foreColor = 0, backColor = 0;
+    
+        while(Math.abs(foreColor - backColor) < 10) {
+            foreColor = Math.floor(Math.random() * 255);
+            backColor = Math.floor(Math.random() * 255);
+        }
+
+        this.setColors(foreColor, backColor);
+    }
+
+    // replaceHistState: save the clock state into the URL query string.
+    replaceHistState(foreColor, backColor, numberType) {
+        const params = new URLSearchParams();
+        params.set('fg', foreColor);
+        params.set('bg', backColor);
+        params.set('mode', numberType);
+        window.history.replaceState({}, '', '?'+params.toString());
+    }
+
+    // isNumeric: qualify input for numeric
+    isNumeric(num) {
+        return !isNaN(num) && num !== undefined;
+    }
+
+}
--- scripts/analog-clock.js
+++ scripts/analog-clock.js
@@ -1,4 +1,4 @@
-const VERSION = '0.0.3';
+const VERSION = '0.0.4';
 
 var running = true;
 var roundedMinSec = false;  // "Direct drive" movement of minute and second hands in intervals.
@@ -7,101 +7,21 @@ var numberType = -1;        // type of numbers used in face. default is none.
 //var tickTime = 200;
 //window.onload = () => setInterval(() => tick(), tickTime);
 
+document.title += ' v' + VERSION;
+
 window.onload = () => {
-    colorize()
-    numberFill();
+    let clock1 = new AnalogClock();
+    clock1.runClock();
 };
 
-function tick() {
-    if (!running) return false;
-    var midnight = new Date();
-    midnight.setHours(0,0,0,0);
-    var ts = new Date().getTime() - midnight.getTime();
-    var pct = ts / 86400000;
-    var hr = pct * 24;
-    var rotation = (720 * pct) - 90;
-    
-    var min = ts / 1000 / 60 % 60;
-    if (roundedMinSec) min = Math.floor(min);
-    var minRotation = 360 * (min / 60) - 90;
-    
-    var sec = ts % 60000 / 1000;
-    if (roundedMinSec) sec = Math.floor(sec);
-    var secRotation = 360 * (sec / 60) - 90;
-    
-    document.querySelector('.hand-hour').style.transform = 'rotate('+rotation+'deg)';
-    document.querySelector('.hand-minute').style.transform = 'rotate('+minRotation+'deg)';
-    document.querySelector('.hand-second').style.transform = 'rotate('+secRotation+'deg)';
-    
-    // text on hands.
-
-    var hNum = numOnHands(hr, min, sec);
-    document.querySelector('.hand-hour').textContent = hNum[0];
-    document.querySelector('.hand-minute').textContent = hNum[1];
-    document.querySelector('.hand-second').textContent = hNum[2];
-    window.requestAnimationFrame(tick);
-}
-
-function numOnHands(hr, min, sec) {
-    switch(numberType) {
-        case 1: return [
-            sig3(Math.round(hr * 1000) / 1000),
-            sig3(Math.round(min * 1000) / 1000),
-            sig3(sec)
-        ];
-        case 2: return [decToRoman(hr), decToRoman(min), decToRoman(sec)];
-        case 3: return [decToBin(hr), decToBin(min), decToBin(sec)];
-
-        default: return ['', '', ''];
-    }
-}
-
-// decToRoman: convert a decimal to a roman numeral, up to 60.
-// first number is empty because roman numerals do not have a concept of zero.
-function decToRoman(num) {
-    var romans = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX', 'XXI', 'XXII', 'XXIII', 'XXIV', 'XXV', 'XXVI', 'XXVII', 'XXVIII', 'XXIX', 'XXX', 'XXXI', 'XXXII', 'XXXIII', 'XXXIV', 'XXXV', 'XXXVI', 'XXXVII', 'XXXVIII', 'XXXIX', 'XL', 'XLI', 'XLII', 'XLIII', 'XLIV', 'XLV', 'XLVI', 'XLVII', 'XLVIII', 'XLIX', 'L', 'LI', 'LII', 'LIII', 'LIV', 'LV', 'LVI', 'LVII', 'LVIII', 'LIX', 'LX'];
-    return romans[Math.floor(num)];
-}
-
-// decToBin: convert a decimal to a binary string.
-// courtesy https://stackoverflow.com/questions/9939760/how-do-i-convert-an-integer-to-binary-in-javascript#16155417
-function decToBin(num) {
-    return (num >>> 0).toString(2);
-}
-
-// sig3: keep the number of digits constant so that the numbers don't jump around
-// addLeadingZero: add a zero to all single-digit leading numbers
-var addLeadingZero = false;
-function sig3(num) {
-    num = num + '';
-    var str = ''; // 00.000
-    var numArr = num.split('.');
-    str = numArr[0].length <= 1 && addLeadingZero? '0' + numArr[0] : numArr[0];
-    str = str + '.';
-
-    for (var x = 0; x < 3; x++) {
-        try {
-        numEl = numArr[1] === undefined? 0 : numArr[1][x];
-        str = numEl? str + '' + numEl : str + '0';
-        }catch(e) {
-            console.log(numArr);
-        }
-    }
-
-    return str;
-
-}
-
-window.requestAnimationFrame(tick);
-
+/*
 document.onclick = () => {
     //roundedMinSec = !roundedMinSec;
     //toggleStyle();
+    numberFill();
     colorize();
+    replaceHistState();
 }
-
-document.title += ' v' + VERSION;
-
 
 // toggleStyle: toggle between black/white style (disabled since 0.0.2)
 var ssHrefs = ['bg-white.css', 'bg-black.css'];
@@ -109,53 +29,6 @@ var styleIndex = 0;
 function toggleStyle() {
     styleIndex = styleIndex >= 1? 0 : 1
     document.getElementById('colors').href = 'styles/' + ssHrefs[styleIndex];
-}
+}*/
 
-// colorize: Give the watch face, hands and random colors that aren't too close to one another.
-function colorize() {
-    numberFill();
 
-    var foreColor = 0;
-    var backColor = 0;
-
-    while(Math.abs(foreColor - backColor) < 10) {
-        foreColor = Math.random() * 255;
-        backColor = Math.random() * 255;
-    }
-
-    var squares = document.querySelectorAll('.square');
-    for (square of squares) {
-        square.style.backgroundColor = 'hsl('+foreColor+',50%,50%)';
-    }
-
-    var hands = document.querySelectorAll('.hand');
-    for (hand of hands) {
-        hand.style.backgroundColor = 'hsl('+foreColor+',20%,50%)';
-    }
-
-    backColor = Math.floor(backColor);
-    document.body.style.backgroundImage = 'linear-gradient(to bottom right, hsl('+backColor+',50%,80%), hsl('+backColor+',50%,50%))';
-}
-
-// numberFill: Place numbers into the clock squares based on a random selection.
-// The first option has no numbers on the clock squares.
-// On clock faces with Roman numerals, IIII is used more commonly than IV. https://www.bernardwatch.com/Glossary-of-Watch-Terms
-// This app uses IIII on the numerals and IV on the hand numbers. 
-var prevNumber = -1;
-function numberFill() {
-    numbers = [
-        ['', '', '', '', '', '', '', '', '', '', '', ''],
-        ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
-        ['I', 'II', 'III', 'IIII', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII'],
-        ['1', '10', '11', '100', '101', '110', '111', '1000', '1001', '1100', '1101', '1110']
-    ];
-
-    while (numberType == prevNumber) numberType = Math.floor(Math.random() * numbers.length);
-
-    var squares = document.querySelectorAll('.square');
-    // squares.length - 1 avoids overwriting the 'hub' square with its link
-    for (var x = 0; x < squares.length - 1; x++) {
-        squares[x].textContent = numbers[numberType][x];
-    }
-    prevNumber = numberType;
-}
